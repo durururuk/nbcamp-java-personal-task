c30b3c7bde4b6be032452eaede4c5863b6ea6b67
durururuk <zkdltms0913@gmail.com>
2024-07-24 11:30:38 +0900

    입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int result = 0;
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
@@ -19,6 +20,36 @@ public class App {
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
         char symbol = sc.next().charAt(0);
         sc.nextLine();
+
+        /* switch를 사용해서 연산을 실행하고 결과값을 출력합니다..*/
+        switch (symbol) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case 'X':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("0으로는 나눌 수 없습니다.");
+                    break;
+                } else {
+                    result = num1 / num2;
+                    break;
+                }
+
+            default:
+                System.out.println("입력이 올바르지 않습니다.");
+                break;
+
+
+        }
+        System.out.println("결과: " + result);
 
     }
 }
