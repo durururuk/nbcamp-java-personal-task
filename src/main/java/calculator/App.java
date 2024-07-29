07aaa8a4b82838e0a7ac19874161691503af652d
durururuk <zkdltms0913@gmail.com>
2024-07-29 16:18:53 +0900

    7. Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,52 +7,89 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double result = 0;
-        int resultCount = 0;
         Calculator cal = new Calculator();
         int num1 = 0;
         int num2 = 0;
+        int radius = 0;
+        boolean isCircle = false;
 
-        while(true) {
+            //전체 반복문
+            while (true) {
+                isCircle = false;
+                //입력이 올바르지 않으면 올바를 때까지 반복
+                while (true) {
+                    System.out.println("'원의 넓이'를 계산할지 '사칙연산'을 계산할지 입력해주세요.");
+                    String whatCal = sc.nextLine();
 
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            num1 = sc.nextInt();
-            sc.nextLine();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = sc.nextInt();
-            sc.nextLine();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
-            sc.nextLine();
+                    if (whatCal.equals("사칙연산")) {
+                        //사칙연산 수행 후 출력
+                        System.out.print("첫 번째 숫자를 입력하세요: ");
+                        num1 = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("두 번째 숫자를 입력하세요: ");
+                        num2 = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("사칙연산 기호를 입력하세요: ");
+                        char operator = sc.nextLine().charAt(0);
+                        try {
+                            result = cal.calculate(num1, num2, operator);
+                        } catch (InvalidOperatorException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println("결과: " + result);
+                        System.out.println();
+                        cal.setQueue("fbo", result);
+                        break;
 
 
-            try {
-                result = cal.calculate(num1,num2,operator);
-            } catch (InvalidOperatorException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("결과: " + result);
-            System.out.println();
+                        //원의 넓이 계산 후 출력
+                    } else if (whatCal.equals("원의 넓이")) {
+                        isCircle = true;
+                        System.out.println("원의 반지름을 입력해주세요.");
+                        radius = sc.nextInt();
+                        sc.nextLine();
+                        result = cal.calculate(radius);
+                        System.out.println("결과: " + result);
+                        System.out.println();
+                        cal.setQueue("circle", result);
+                        cal.getQueue("circle");
+                        break;
+
+
+                    } else {
+                        System.out.println("입력이 올바르지 않습니다.");
+                    }
+                }
+
+
+
+
 
             System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제) : ");
             String remove = sc.nextLine();
             if (remove.equals("remove")) {
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제합니다.");
-                cal.pollResult();
-                if (resultCount == 0) {continue;} else{resultCount--;}
+                //원의 넓이를 계산했을 경우
+                if(isCircle == true) {
+                    System.out.println("가장 먼저 저장된 원의 넓이 연산 결과를 삭제합니다.");
+                    cal.pollResult("circle");
+                }   //사칙연산을 수행했을 경우
+                    else {
+                    System.out.println("가장 먼저 저장된 사칙 연산 연산 결과를 삭제합니다.");
+                    cal.pollResult("FBO");
+                }
+
             } else {
                 System.out.println("가장 먼저 저장된 연산 결과를 삭제하지 않습니다.");
             }
 
-            System.out.println("현재 저장된 결과값의 갯수 : " + (resultCount + 1));
-                resultCount++;
-            System.out.println();
-
-            System.out.print("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회) : ");
+            if(isCircle == false) {
+            System.out.print("저장된 사칙연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회) : ");
             String inquiry = sc.nextLine();
             if (inquiry.equals("inquiry")) {
-                cal.inQuiry();
-            }
+                cal.getQueue("FBO");
+            } }
 
+            System.out.println();
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
             /* exit을 입력 받으면 반복 종료 */
             String ctn = sc.nextLine();
@@ -62,7 +99,6 @@ public class App {
             } else {
                 System.out.println("계산을 게속합니다.");
                 System.out.println();
-                break;
             }
 
         }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,67 +4,89 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Calculator {
-    private double result;
     private final Queue<Double> resultQueue;
+    private final Queue<Double> circleResultQueue;
+    private static final double pi = 3.141592;
+    //외부에서 resultQueue와 circleResultQueue를 수정할 수 없도록 private final로 설정
+    // pi의 경우에는 절대 불변의 상수임으로 private static final로 설정
+
 
     public Calculator() {
-        this.result = 0;
         this.resultQueue = new LinkedList<>();
+        this.circleResultQueue = new LinkedList<>();
     }
 
-    public void pollResult() {
-        resultQueue.poll();
-    }
-
-    public void inQuiry() {
-        System.out.println("저장돤 결괏값은 다음과 같습니다..");
-        System.out.print("[ ");
-        for (Double resultValue : resultQueue) {
-            System.out.print(resultValue + ", ");
+    //가장 먼저 저장된 결과값을 삭제하기
+    public void pollResult(String what) {
+        if(what.equals("FBO")) {
+            resultQueue.poll();
+        } else {
+            circleResultQueue.poll();
         }
-        System.out.print("]");
-        System.out.println();
+
+    }
+
+    //저장된 결과값 보기
+    public void getQueue(String what) {
+        if (what.equals("FBO")) {
+            System.out.println("저장돤 결괏값은 다음과 같습니다..");
+            System.out.print("[ ");
+            for (Double resultValue : resultQueue) {
+                System.out.print(resultValue + ", ");
+            }
+            System.out.print("]");
+            System.out.println();
+        } else {
+            System.out.println("저장돤 결괏값은 다음과 같습니다..");
+            System.out.print("[ ");
+            for (Double resultValue : circleResultQueue) {
+                System.out.print(resultValue + ", ");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+    }
+
+    //결과값 저장하기
+    public void setQueue(String what, double result) {
+        if (what.equals("circle")) {
+            circleResultQueue.add(result);
+        } else{
+            resultQueue.add(result);
+        }
     }
 
 
 
 
-    public double calculate (int firstNumber, int secondNumber, char operator) throws InvalidOperatorException {
+
+    //인스턴트 필드 사용 없이 입력된 매개변수만 가지고 계산을 실행함으로 static 메서드로 설정
+    public static double calculate (int firstNumber, int secondNumber, char operator) throws InvalidOperatorException {
         switch (operator) {
             case '+' :
-                result = firstNumber + secondNumber;
-                resultQueue.add(result);
-                return result;
+               return firstNumber + secondNumber;
 
             case '-' :
-                result = firstNumber - secondNumber;
-                resultQueue.add(result);
-                return result;
+                return firstNumber - secondNumber;
 
             case '*' :
-                result = firstNumber * secondNumber;
-                resultQueue.add(result);
-                return result;
+                return firstNumber * secondNumber;
 
             case '/' :
                 try {
-                    result = (double) firstNumber / secondNumber;
+                    return (double) firstNumber / secondNumber;
 
                 } catch(ArithmeticException e) {
                     System.out.println("0으로는 나눌 수 없습니다.");
                     return Double.NaN;
                 }
 
-                    resultQueue.add(result);
-                    return result;
-
-
-
-
             default:
                 throw new InvalidOperatorException("입력이 올바르지 않습니다.");
-
-
         }
+    }
+
+    public static double calculate(int radius) {
+        return (double) radius * radius * pi;
     }
 }
